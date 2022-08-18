f6f09eb7e0180ebe763b68fdcfd5a39b5ca5ba01
Luther Tchofo-Safo <nintendobserver@gmail.com>
2022-08-18 10:51:33 +0200

    Yesterday.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating 9 fake locations..."
+9.times do
+  Flat.create!(
+    name: Faker::Color.color_name.capitalize,
+    address: Faker::Address.full_address,
+    description: Faker::Quote.famous_last_words,
+    price_per_night: (49..99).to_a.sample,
+    number_of_guests: (1..5).to_a.sample
+  )
+end
+
+puts "Finished!"
